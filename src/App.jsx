709c4383d4211cb0531ad0350039eb070afaa0c3
senior-webdeveloper <rsj19960202@gmail.com>
2022-01-14 18:59:55 +0900

    My hooks

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+import { useState, useEffect } from 'react';
+import ChatAPI from 'react-bell-chat'
+import Board from './Board'
+
+function App(props) {
+
+  const [isOnline, setIsOnline] = useState(null);
+  useEffect(() => {
+    function handleStatusChange(status) {
+      setIsOnline(status.isOnline);
+    }
+    ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
+    return () => {
+      ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
+    };
+  });
+
+  if (isOnline === null) {
+    return 'Loading...';
+  }
+  return isOnline ? 'Online' : 'Offline';
+
+}
+
+export default App;
---
+++ src/Square.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function Square(props) {
+
+}
+
+export default Square;
